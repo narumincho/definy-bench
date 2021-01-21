455eeab41d18874524094f41204fa6d587884549
narumincho <narumincho.starfy@gmail.com>
2021-01-21 16:46:29 +0900

    add jest output file, check encode decode equal

---
+++ test.ts
@@ -0,0 +1,38 @@
+import * as lib from "./lib";
+import { promises as fileSystem } from "fs";
+
+describe("encodeDecodeMatch", () => {
+  it("save file", () => {
+    fileSystem.writeFile(
+      "file/definyType.json",
+      JSON.stringify(lib.definyCoreSchemaAsDefiny)
+    );
+
+    fileSystem.writeFile(
+      "file/definyType.bin",
+      lib.encodeByDefinyCore(lib.definyCoreSchemaAsDefiny)
+    );
+
+    fileSystem.writeFile(
+      "file/proto.bin",
+      lib.encodeProto(lib.definyCoreSchemaAsProtoMessage)
+    );
+  });
+  it("json", () => {
+    expect(
+      lib.decodeByJson(lib.encodeByJSON(lib.definyCoreSchemaAsDefiny))
+    ).toEqual(lib.definyCoreSchemaAsDefiny);
+  });
+  it("definyCore", () => {
+    expect(
+      lib.decodeDefinyCode(lib.encodeByDefinyCore(lib.definyCoreSchemaAsDefiny))
+    ).toEqual(lib.definyCoreSchemaAsDefiny);
+  });
+  it("protocolBuffers", () => {
+    expect(
+      lib
+        .decodeProto(lib.encodeProto(lib.definyCoreSchemaAsProtoMessage))
+        .toObject()
+    ).toEqual(lib.definyCoreSchemaAsProtoMessage.toObject());
+  });
+});
